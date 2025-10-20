46a9a587927fb9dd7596dd7b8f2b2f0b38d76596
Okwir Moses <60920202+hokwirmoki@users.noreply.github.com>
2025-10-20 17:44:49 +0300

    file update

--- hostel-backend/controllers/authController.js
+++ hostel-backend/controllers/authController.js
@@ -46,3 +46,12 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
+
+exports.me = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
