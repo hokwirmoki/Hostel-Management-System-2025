f5cd582c2f2e0907dbdc20e3a3c951a4dd6df37f
kauvincentjames <kauvincentj@gmail.com>
2025-10-22 09:40:16 +0300

    Added Admin file

---
+++ hostel-backend/middleware/isAdmin.js
@@ -0,0 +1,8 @@
+// middleware/isAdmin.js
+module.exports.isAdmin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({ message: "Access denied. Admins only." });
+  }
+};
